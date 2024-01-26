9379b6864f9bb33fedfaf47eaaae89daf4beed3f
Tlbhs <125154179+Tlbhs@users.noreply.github.com>
2024-01-27 01:15:14 +0330

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,9 @@ import { connect } from 'cloudflare:sockets';
 // t.me/P_tech2024 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = '2cd61524-9843-43ab-8a0a-df4a744bb859';
+let userID = '82a79361-6a9d-4436-a296-40ed20906604
+
+';
 
 const proxyIPs = ['mtn.ircf.space', 'mkh.ircf.space', 'mci.ircf.space', 'rtl.ircf.space'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
